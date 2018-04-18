b9bf08d0dedac287fc573a96d7591bd7440b1d72
Naha Fujii <snao178@gmail.com>
2018-04-19 00:16:02 +0900

    Hello World末尾にI'm a little programmer.の文を追加。

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!I'm a little programmer." << std::endl;
     std::cout << "こんにちは世界!" << std::endl;
     std::cout << "私はプログラマです。" << std::endl;
     return 0;
