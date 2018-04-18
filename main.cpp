e0a9ffc7e81c981bb91486d1c2ba8fb443c7e7f9
Natti970 <snao178@gmail.com>
2018-04-19 00:22:31 +0900

    　5行目末尾によろしくお願いします。を追加

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "こんにちは世界!私は駆け出しのプログラマーです。" << std::endl;
+    std::cout << "こんにちは世界!私は駆け出しのプログラマーです。よろしくお願いします。" << std::endl;
     std::cout << "私はプログラマです。" << std::endl;
     return 0;
 }
